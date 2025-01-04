f175045591517c3b5103d3dc12e7c09b5ac1c0a0
Asthere0n <cristiansanchezsonido@gmail.com>
2025-01-04 13:52:24 +0000

    Implement Linked List and update HashMap
    - Added a new Linked List implementation in `utilities/Linked-List.js`
    - Updated `app.js` to integrate Linked List with existing functionality
    - Modified `utilities/HashMap.js` to use Linked List for collision handling

--- utilities/HashMap.js
+++ utilities/HashMap.js
@@ -1,20 +1,32 @@
 class HashMap {
     constructor(loadFactor, capacity = 16) {
         this.loadFactor = loadFactor,
-        this.capacity = capacity
+        this.capacity = capacity,
+        this.bucketList = []
      }
 
     hash(key) {
+        // Generates a hash for a given key 
         let hashCode = 0
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = (primeNumber * hashCode + key.charCodeAt(i)%16);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i))%this.capacity;
         }
         return hashCode;
     }
-    
-    set(key, value){
 
+    set(key, value){
+        // Introduces a new value in a key bucket, if the key already exists it will overwrite.
+        const hashKey = this.hash(key)
+        const newBucket = {head: {key : value}}
+        
+        // When hash-key is the same but the keys are different we have a collision, we solve it using a linked list.
+        if (this.bucketList[hashKey] == undefined || this.bucketList[hashKey][0] === key){
+            this.bucketList[hashKey] = newBucket
+        } else {
+            this.bucketList[hashKey][2] = key
+            this.bucketList[hashKey].push(newBucket)
+        }
     }
 }
 
---
+++ utilities/Linked-List.js
@@ -0,0 +1,14 @@
+class ListNode {
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(head = null){
+        this.head = head
+    }
+}
+
+export { ListNode, LinkedList }
